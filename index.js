7f45ddc402b4040471284d14314c473fe1713fdb
Shivam Balwani <48817020+balwanishivam@users.noreply.github.com>
2020-04-29 05:05:05 +0530

    Keys Play Added

--- index.js
+++ index.js
@@ -10,8 +10,59 @@ window.addEventListener('load',()=>{
         "6860d3",
         "60b2d3"
     ];
-    console.log(sounds); 
+    console.log(sounds);
     console.log(visual);
+    
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 65) {
+            sounds[0].currentTime=0;
+            sounds[0].play();
+            index=0;
+            createBubbles(index);
+        }
+      });
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 69) {
+            sounds[1].currentTime=0;
+            sounds[1].play();
+            index=1;
+            createBubbles(index);
+        }
+      });
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 68) {
+            sounds[2].currentTime=0;
+            sounds[2].play();
+            index=2;
+            createBubbles(index);
+        }
+      });
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 83) {
+            sounds[4].currentTime=0;
+            sounds[4].play();
+            index=4;
+            createBubbles(index);
+        }
+      });
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 87) {
+            sounds[5].currentTime=0;
+            sounds[5].play();
+            index=5;
+            createBubbles(index);
+        }
+      });
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode == 81) {
+            sounds[3].currentTime=0;
+            sounds[3].play();
+            index=3;
+            createBubbles(index);
+        }
+      });
+     
+    
     pads.forEach((pad,index) =>{
           pad.addEventListener('click',function(){
               sounds[index].currentTime=0;
@@ -26,7 +77,7 @@ window.addEventListener('load',()=>{
         bubble.style.backgroundColor=color[index];
         bubble.style.animation="jump 1s ease";
         bubble.addEventListener('animationend',function(){
-            visual.removeChild(this)
+            visual.removeChild(this);
         });
-    };
+    };  
 });
